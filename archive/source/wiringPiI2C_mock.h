b929b483be9a5018511e339a61b92c8f5c4917e7
William Schmitt <willj@schmittnet.com>
2016-08-22 22:14:50 -0400

    Removing old c code, which originated from arduino code.

--- archive/source/wiringPiI2C_mock.h
+++
@@ -1,8 +0,0 @@
-#ifndef wiringPiI2C_mock_h
-#define wiringPiI2C_mock_h
-
-int wiringPiI2CSetup (int devId);
-int wiringPiI2CRead (int fd);
-int wiringPiI2CWrite (int fd, int data);
-
-#endif //wiringPiI2C_mock_h
