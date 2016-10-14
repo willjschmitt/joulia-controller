17a1b218f6a102334708e4ff3daf4d34c1983f86
William Schmitt <willj@schmittnet.com>
2016-10-13 21:56:50 -0400

    RTD sensor communicates via i2c.

---
+++ measurement/arduino.py
@@ -0,0 +1,18 @@
+"""Utility functions for interacting with arduino over I2C"""
+
+import smbus
+
+def analog_read(channel):
+    """Reads the counts value broadcast from an arduino at the indicated
+    channel.
+
+    Args:
+        channel: The analog pin to read at.
+    """
+    bus = smbus.SMBus(0)
+    address = 0x0A
+
+    counts1 = bus.read_byte_data(address, 0)
+    counts2 = bus.read_byte_data(address, 1)
+    counts = (counts1 << 8) + counts2
+    return counts
