962a747eacf64e413eb8c64c114dbb74ce08bf85
William Schmitt <willj@schmittnet.com>
2017-03-18 21:05:11 -0700

    Coverage for aruduino (with refactoring).

--- measurement/arduino.py
+++ measurement/arduino.py
@@ -1,23 +1,30 @@
 """Utility functions for interacting with arduino over I2C"""
 
-from utils import GPIO_MOCK_API_ACTIVE
 
-if not GPIO_MOCK_API_ACTIVE:
-    import smbus
+class AnalogReader(object):
+    """Class for reading from an Arduino device over I2C for analog
+    measurements.
 
-def analog_read(channel):
-    """Reads the counts value broadcast from an arduino at the indicated
-    channel.
-
-    Args:
-        channel: The analog pin to read at.
+    Attributes:
+        i2c_bus: An i2c bus to be used for communicating with the Arduino. In
+            production, usually is an instance of smbus.SMBus.
+        address: I2C address for the arduino device on the i2c_bus.
     """
-    bus = smbus.SMBus(1)
-    address = 0x0A
 
-    bus.write_byte(address, channel)
+    def __init__(self, i2c_bus, address):
+        self.i2c_bus = i2c_bus
+        self.address = address
 
-    counts1 = bus.read_byte_data(address, 0)
-    counts2 = bus.read_byte_data(address, 1)
-    counts = (counts1 << 8) + counts2
-    return counts
+    def read(self, channel):
+        """Reads the counts value broadcast from an arduino at the indicated
+        channel.
+
+        Args:
+            channel: The analog pin to read at.
+        """
+        self.i2c_bus.write_byte(self.address, channel)
+
+        counts1 = self.i2c_bus.read_byte_data(self.address, 0)
+        counts2 = self.i2c_bus.read_byte_data(self.address, 1)
+        counts = (counts1 << 8) + counts2
+        return counts
