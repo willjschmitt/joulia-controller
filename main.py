82f3467d1383778529b0b7df284a805740d0c47a
William Schmitt <willj@schmittnet.com>
2017-03-19 15:52:22 -0700

    Main cleanup.

--- main.py
+++ main.py
@@ -1,22 +1,19 @@
-'''
-Created on Apr 5, 2016
-
-@author: William
-'''
+"""Main code for launching a Brewhouse joulia-controller."""
 import logging.config
 from tornado import ioloop, gen
-from brewery import brewing
+from brewery.brewhouse import Brewhouse
 import settings
 
 logging.basicConfig(level=logging.DEBUG)
 logging.config.dictConfig(settings.LOGGING_CONFIG)
 LOGGER = logging.getLogger(__name__)
 
+
 def main():
     """Main routine is for running as standalone controller on embedded
     hardware. Loads settings from module and env vars, and launches a
     controller instance."""
-    brewing.Brewhouse(AUTHTOKEN=settings.AUTHTOKEN)
+    Brewhouse()
     LOGGER.info('Brewery initialized.')
 
     ioloop.IOLoop.instance().start()
